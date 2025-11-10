72c857aae0632e756a497ae2b5623dcd2273aaf1
Denis Matsuga <HitokiriDeepWeb@yandex.ru>
2025-11-10 13:48:16 +0400

    Rename database_lifecycle module

--- src/infrastructure/database/postgresql/__init__.py
+++ src/infrastructure/database/postgresql/__init__.py
@@ -1,12 +1,12 @@
 from .adapter import PostgreSQLAdapter
 from .config import ConnectionConfig, ConnectionPoolConfig
-from .database_lifecycle import PostgreSQLUniprotLifecycle
 from .setup_config import (
     adjust_workers_by_db_connection_limit,
     get_available_connections_amount,
     setup_connection_pool_config,
     setup_queue_config,
 )
+from .uniprot_lifecycle import PostgreSQLUniprotLifecycle
 
 __all__ = [
     "PostgreSQLUniprotLifecycle",
