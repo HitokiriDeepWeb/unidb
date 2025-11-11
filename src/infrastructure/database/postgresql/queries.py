af7926fb3238a5ab7f5c6cc44f873c0c0f8bcee6
Denis Matsuga <HitokiriDeepWeb@yandex.ru>
2025-11-11 15:37:20 +0400

    Rename local only used constants

--- src/infrastructure/database/postgresql/queries.py
+++ src/infrastructure/database/postgresql/queries.py
@@ -1,7 +1,7 @@
 from domain.entities import Tables
 
 # Clean memory from table data and indexes.
-TRUNCATE_ALL_TABLES_QUERY: str = f"""
+_TRUNCATE_ALL_TABLES_QUERY: str = f"""
                                  TRUNCATE TABLE
                                  {Tables.METADATA},
                                  {Tables.TAXONOMY},
@@ -10,9 +10,9 @@ TRUNCATE_ALL_TABLES_QUERY: str = f"""
                                  CASCADE
                                  """
 
-DROP_TYPE_SOURCE_QUERY: str = """DROP TYPE IF EXISTS sequence_source CASCADE"""
+_DROP_TYPE_SOURCE_QUERY: str = """DROP TYPE IF EXISTS sequence_source CASCADE"""
 
-DROP_IDXS_QUERY: tuple = (
+_DROP_IDXS_QUERY: tuple = (
     f"""DROP INDEX IF EXISTS ncbi_organism_id_{Tables.UNIPROT}_idx""",
     f"""DROP INDEX IF EXISTS {Tables.UNIPROT}_source_idx""",
     """DROP INDEX IF EXISTS trgm_sequence_idx""",
@@ -24,31 +24,31 @@ DROP_IDXS_QUERY: tuple = (
 )
 
 # Create extension to create gin index on sequence later.
-CREATE_TRGM_EXTENSION_QUERY: str = """CREATE EXTENSION IF NOT EXISTS pg_trgm"""
+_CREATE_TRGM_EXTENSION_QUERY: str = """CREATE EXTENSION IF NOT EXISTS pg_trgm"""
 
 # If uniprot_kb exist already and we need to update -
 # drop all constraints and clean all the tables.
-DROP_CONSTRAINTS_UNIPROT_KB_QUERY: str = f"""
+_DROP_CONSTRAINTS_UNIPROT_KB_QUERY: str = f"""
                                          ALTER TABLE IF EXISTS {Tables.UNIPROT}
                                          DROP CONSTRAINT IF EXISTS {Tables.UNIPROT}_pkey
                                          """
 
-DROP_CONSTRAINTS_TAXONOMY_QUERY: str = f"""
+_DROP_CONSTRAINTS_TAXONOMY_QUERY: str = f"""
                                ALTER TABLE IF EXISTS {Tables.TAXONOMY}
                                DROP CONSTRAINT IF EXISTS {Tables.TAXONOMY}_pkey CASCADE,
                                DROP CONSTRAINT IF EXISTS unique_tax_name
                                """
 
-DROP_CONSTRAINTS_LINEAGE_QUERY: str = f"""
+_DROP_CONSTRAINTS_LINEAGE_QUERY: str = f"""
     ALTER TABLE IF EXISTS {Tables.LINEAGE}
     DROP CONSTRAINT IF EXISTS taxon_{Tables.LINEAGE}_idpair_pkey,
     DROP CONSTRAINT IF EXISTS unique_taxon_{Tables.LINEAGE}_idpair
     """
 
 # Create all tables without constraints to load data faster.
-DROP_METADATA_QUERY: str = f"""DROP TABLE IF EXISTS {Tables.METADATA} CASCADE"""
+_DROP_METADATA_QUERY: str = f"""DROP TABLE IF EXISTS {Tables.METADATA} CASCADE"""
 
-CREATE_METADATA_QUERY: str = f"""
+_CREATE_METADATA_QUERY: str = f"""
                              CREATE TABLE IF NOT EXISTS {Tables.METADATA}(
                              data_source VARCHAR(100),
                              data_license VARCHAR(100),
@@ -56,33 +56,33 @@ CREATE_METADATA_QUERY: str = f"""
                              attribution_required VARCHAR(3)
                              )
                              """
-DROP_TAXONOMY_QUERY: str = f"""DROP TABLE IF EXISTS {Tables.TAXONOMY} CASCADE"""
+_DROP_TAXONOMY_QUERY: str = f"""DROP TABLE IF EXISTS {Tables.TAXONOMY} CASCADE"""
 
-CREATE_TAXONOMY_QUERY: str = f"""
+_CREATE_TAXONOMY_QUERY: str = f"""
                              CREATE TABLE IF NOT EXISTS {Tables.TAXONOMY}(
                              rank VARCHAR(60),
                              ncbi_taxon_id INT,
                              tax_name VARCHAR(1000))
                              """
 
-DROP_LINEAGE_QUERY: str = f"""DROP TABLE IF EXISTS {Tables.LINEAGE} CASCADE"""
+_DROP_LINEAGE_QUERY: str = f"""DROP TABLE IF EXISTS {Tables.LINEAGE} CASCADE"""
 
-CREATE_LINEAGE_QUERY: str = f"""
+_CREATE_LINEAGE_QUERY: str = f"""
                             CREATE TABLE IF NOT EXISTS {Tables.LINEAGE}(
                             ncbi_taxon_id INT,
                             ncbi_lineage_id INT)
                             """
 
-DROP_MERGED_ID_QUERY: str = f"""DROP TABLE IF EXISTS {Tables.MERGED} CASCADE"""
+_DROP_MERGED_ID_QUERY: str = f"""DROP TABLE IF EXISTS {Tables.MERGED} CASCADE"""
 
-CREATE_MERGED_ID_QUERY: str = f"""
+_CREATE_MERGED_ID_QUERY: str = f"""
                               CREATE TABLE IF NOT EXISTS {Tables.MERGED}(
                               deprecated_ncbi_taxon_id INT,
                               current_ncbi_taxon_id INT
                               )
                               """
 
-CREATE_SOURCE_ENUM_QUERY: str = """
+_CREATE_SOURCE_ENUM_QUERY: str = """
                                 CREATE TYPE sequence_source AS ENUM(
                                  'sp',
                                  'tr',
@@ -91,9 +91,9 @@ CREATE_SOURCE_ENUM_QUERY: str = """
                                 )
                                 """
 
-DROP_UNIPROT_KB_QUERY: str = f"""DROP TABLE IF EXISTS {Tables.UNIPROT} CASCADE"""
+_DROP_UNIPROT_KB_QUERY: str = f"""DROP TABLE IF EXISTS {Tables.UNIPROT} CASCADE"""
 
-CREATE_UNIPROT_KB_QUERY: str = f"""
+_CREATE_UNIPROT_KB_QUERY: str = f"""
                                CREATE TABLE IF NOT EXISTS {Tables.UNIPROT}(
                                source sequence_source,
                                is_reviewed bool,
@@ -105,12 +105,12 @@ CREATE_UNIPROT_KB_QUERY: str = f"""
                                sequence TEXT)
                                """
 
-CREATE_TAXONOMY_TAX_NAME_IDX_QUERY: str = f"""
+_CREATE_TAXONOMY_TAX_NAME_IDX_QUERY: str = f"""
                                             CREATE UNIQUE INDEX unique_tax_name_idx
                                             ON {Tables.TAXONOMY} (tax_name)
                                             """
 
-ADD_CONSTRAINTS_TAXONOMY_QUERY: str = f"""
+_ADD_CONSTRAINTS_TAXONOMY_QUERY: str = f"""
                                       ALTER TABLE {Tables.TAXONOMY}
                                       ADD CONSTRAINT taxonomy_pkey
                                       PRIMARY KEY (ncbi_taxon_id),
@@ -118,14 +118,14 @@ ADD_CONSTRAINTS_TAXONOMY_QUERY: str = f"""
                                       UNIQUE USING INDEX unique_tax_name_idx
                                       """
 
-ADD_NOT_NULL_CONSTRAINT_TAXONOMY_QUERY: str = f"""
+_ADD_NOT_NULL_CONSTRAINT_TAXONOMY_QUERY: str = f"""
                                               ALTER TABLE {Tables.TAXONOMY}
                                               ALTER COLUMN rank SET NOT NULL,
                                               ALTER COLUMN tax_name SET NOT NULL
                                               """
 
 # Create indexes temporarily for faster performance.
-CREATE_TMP_IDXS_QUERY: tuple = (
+_CREATE_TMP_IDXS_QUERY: tuple = (
     f"""CREATE INDEX IF NOT EXISTS {Tables.MERGED}_tmp_current_ncbi_taxon_id
         ON {Tables.MERGED} (current_ncbi_taxon_id)""",
     f"""CREATE INDEX IF NOT EXISTS {Tables.MERGED}_tmp_deprecated_ncbi_taxon_id
@@ -136,7 +136,7 @@ CREATE_TMP_IDXS_QUERY: tuple = (
 
 # Find all deprecated ncbi IDs from uniprot tables and substitute them with current ones
 # using table 'merged_id'.
-SUBSTITUTE_OUTDATED_NCBI_IDS_IN_UNIPROT_KB_QUERY: str = f"""
+_SUBSTITUTE_OUTDATED_NCBI_IDS_IN_UNIPROT_KB_QUERY: str = f"""
         UPDATE {Tables.UNIPROT}
         SET ncbi_organism_id = current_ncbi_taxon_id
         FROM {Tables.MERGED}
@@ -151,7 +151,7 @@ SUBSTITUTE_OUTDATED_NCBI_IDS_IN_UNIPROT_KB_QUERY: str = f"""
 
 # Since ncbi ids in uniprot tables and ids from 'taxonomy' table are the same now -
 # create foreign keys.
-CREATE_NCBI_ID_FKEY_UNIPROT_KB: str = f"""
+_CREATE_NCBI_ID_FKEY_UNIPROT_KB: str = f"""
     ALTER TABLE {Tables.UNIPROT}
     ADD CONSTRAINT {Tables.UNIPROT}_ncbi_organism_id_fkey
     FOREIGN KEY (ncbi_organism_id)
@@ -160,7 +160,7 @@ CREATE_NCBI_ID_FKEY_UNIPROT_KB: str = f"""
     """
 
 # Add not null constraints to uniprot_kb.
-ADD_NOT_NULL_UNIPROT_KB_QUERY: str = f"""
+_ADD_NOT_NULL_UNIPROT_KB_QUERY: str = f"""
                                 ALTER TABLE {Tables.UNIPROT}
                                 ALTER COLUMN source SET NOT NULL,
                                 ALTER COLUMN is_reviewed SET NOT NULL,
@@ -172,31 +172,31 @@ ADD_NOT_NULL_UNIPROT_KB_QUERY: str = f"""
                                 """
 
 # Drop indexes that we don't need anymore.
-DROP_UNUSED_IDXS_QUERY: tuple = (
+_DROP_UNUSED_IDXS_QUERY: tuple = (
     f"""DROP INDEX IF EXISTS {Tables.MERGED}_tmp_current_ncbi_taxon_id""",
     f"""DROP INDEX IF EXISTS {Tables.MERGED}_tmp_deprecated_ncbi_taxon_id""",
     f"""DROP INDEX IF EXISTS {Tables.UNIPROT}_tmp_ncbi_organism_id""",
 )
 
-ADD_PK_CONSTRAINT_UNIPROT_KB_QUERY: str = f"""
+_ADD_PK_CONSTRAINT_UNIPROT_KB_QUERY: str = f"""
                                      ALTER TABLE {Tables.UNIPROT}
                                      ADD CONSTRAINT {Tables.UNIPROT}_pkey
                                      PRIMARY KEY (accession)
                                      """
 
-CREATE_IDXS_UNIPROT_KB_QUERY: tuple = (
+_CREATE_IDXS_UNIPROT_KB_QUERY: tuple = (
     f"""CREATE INDEX IF NOT EXISTS ncbi_organism_id_{Tables.UNIPROT}_idx
         ON {Tables.UNIPROT} (ncbi_organism_id)""",
     f"""CREATE INDEX IF NOT EXISTS {Tables.UNIPROT}_source ON {Tables.UNIPROT} (source)
         WHERE source != 'tr'""",
 )
 
-CREATE_LINEAGE_IDXS_QUERY: str = f"""
+_CREATE_LINEAGE_IDXS_QUERY: str = f"""
                              CREATE UNIQUE INDEX unique_taxon_{Tables.LINEAGE}_idpair
                              ON {Tables.LINEAGE} (ncbi_lineage_id, ncbi_taxon_id)
                              """
 
-ADD_CONSTRAINTS_LINEAGE_QUERY: str = f"""
+_ADD_CONSTRAINTS_LINEAGE_QUERY: str = f"""
                               ALTER TABLE {Tables.LINEAGE}
                               ADD CONSTRAINT {Tables.LINEAGE}_ncbi_taxon_id_fkey
                               FOREIGN KEY (ncbi_taxon_id)
@@ -214,14 +214,14 @@ ADD_CONSTRAINTS_LINEAGE_QUERY: str = f"""
                               UNIQUE USING INDEX unique_taxon_{Tables.LINEAGE}_idpair
                               """
 
-ADD_NOT_NULL_CONSTRAINTS_LINEAGE_QUERY: str = f"""
+_ADD_NOT_NULL_CONSTRAINTS_LINEAGE_QUERY: str = f"""
                                               ALTER TABLE {Tables.LINEAGE}
                                               ALTER COLUMN ncbi_taxon_id SET NOT NULL,
                                               ALTER COLUMN ncbi_lineage_id SET NOT NULL
                                               """
 
 # Database comments.
-UNIPROT_KB_COMMENTS_QUERY: tuple = (
+_UNIPROT_KB_COMMENTS_QUERY: tuple = (
     f"COMMENT ON TABLE {Tables.UNIPROT} is "
     "'All peptide records (Swiss-Prot, TrEMBL, reviewed isoforms).'",
     f"COMMENT ON COLUMN {Tables.UNIPROT}.source is "
@@ -239,14 +239,14 @@ UNIPROT_KB_COMMENTS_QUERY: tuple = (
     f"COMMENT ON COLUMN {Tables.UNIPROT}.sequence is 'Peptide sequence itself.'",
 )
 
-TAXONOMY_COMMENTS_QUERY: tuple = (
+_TAXONOMY_COMMENTS_QUERY: tuple = (
     f"COMMENT ON TABLE {Tables.TAXONOMY} is 'Taxonomy info.'",
     f"COMMENT ON COLUMN {Tables.TAXONOMY}.ncbi_taxon_id is 'NCBI taxon ID, PK.'",
     f"COMMENT ON COLUMN {Tables.TAXONOMY}.tax_name is 'Taxon name with NCBI taxon ID.'",
     f"COMMENT ON COLUMN {Tables.TAXONOMY}.rank is 'Rank of the taxon.'",
 )
 
-LINEAGE_COMMENTS_QUERY: tuple = (
+_LINEAGE_COMMENTS_QUERY: tuple = (
     f"COMMENT ON TABLE {Tables.LINEAGE} is 'Lineage taxons that correspond organism.'",
     f"COMMENT ON COLUMN {Tables.LINEAGE}.ncbi_taxon_id is "
     "'NCBI taxon ID of the organism that posess lineage taxons FK.'",
@@ -254,7 +254,7 @@ LINEAGE_COMMENTS_QUERY: tuple = (
     "'NCBI lineage taxon ID that are possessed by organism FK.'",
 )
 
-INSERT_INFO_INTO_METADATA: str = f"""
+_INSERT_INFO_INTO_METADATA: str = f"""
                INSERT INTO {Tables.METADATA}(data_source,
                                              data_license,
                                              license_url,
@@ -272,73 +272,73 @@ CREATE_TRGM_IDX_ON_UNIPROT_KB: str = f"""CREATE INDEX trgm_sequence_idx ON
                                            USING GIN(sequence gin_trgm_ops)
                                        """
 
-CREATE_TRGM_IDX_ON_TAXONOMY: str = f"""CREATE INDEX trgm_tax_name_idx ON
+_CREATE_TRGM_IDX_ON_TAXONOMY: str = f"""CREATE INDEX trgm_tax_name_idx ON
                                          {Tables.TAXONOMY}
                                          USING GIN(tax_name gin_trgm_ops)
                                      """
 
 REMOVE_DATABASE_QUERIES: tuple = (
-    DROP_TYPE_SOURCE_QUERY,
-    DROP_IDXS_QUERY,
-    DROP_CONSTRAINTS_UNIPROT_KB_QUERY,
-    DROP_CONSTRAINTS_TAXONOMY_QUERY,
-    DROP_CONSTRAINTS_LINEAGE_QUERY,
-    DROP_METADATA_QUERY,
-    DROP_UNIPROT_KB_QUERY,
-    DROP_TAXONOMY_QUERY,
-    DROP_LINEAGE_QUERY,
-    DROP_MERGED_ID_QUERY,
+    _DROP_TYPE_SOURCE_QUERY,
+    _DROP_IDXS_QUERY,
+    _DROP_CONSTRAINTS_UNIPROT_KB_QUERY,
+    _DROP_CONSTRAINTS_TAXONOMY_QUERY,
+    _DROP_CONSTRAINTS_LINEAGE_QUERY,
+    _DROP_METADATA_QUERY,
+    _DROP_UNIPROT_KB_QUERY,
+    _DROP_TAXONOMY_QUERY,
+    _DROP_LINEAGE_QUERY,
+    _DROP_MERGED_ID_QUERY,
 )
 
 RESET_DATABASE_QUERIES: tuple = (
-    TRUNCATE_ALL_TABLES_QUERY,
+    _TRUNCATE_ALL_TABLES_QUERY,
     REMOVE_DATABASE_QUERIES,
 )
 
 PREPARATION_QUERIES: tuple = (
-    CREATE_TRGM_EXTENSION_QUERY,
-    CREATE_SOURCE_ENUM_QUERY,
+    _CREATE_TRGM_EXTENSION_QUERY,
+    _CREATE_SOURCE_ENUM_QUERY,
 )
 
 TABLE_CREATION_QUERIES: tuple = (
-    CREATE_METADATA_QUERY,
-    CREATE_UNIPROT_KB_QUERY,
-    CREATE_MERGED_ID_QUERY,
-    CREATE_TAXONOMY_QUERY,
-    CREATE_LINEAGE_QUERY,
+    _CREATE_METADATA_QUERY,
+    _CREATE_UNIPROT_KB_QUERY,
+    _CREATE_MERGED_ID_QUERY,
+    _CREATE_TAXONOMY_QUERY,
+    _CREATE_LINEAGE_QUERY,
 )
 
 COMMENT_QUERIES: tuple = (
-    UNIPROT_KB_COMMENTS_QUERY,
-    TAXONOMY_COMMENTS_QUERY,
-    LINEAGE_COMMENTS_QUERY,
-    INSERT_INFO_INTO_METADATA,
+    _UNIPROT_KB_COMMENTS_QUERY,
+    _TAXONOMY_COMMENTS_QUERY,
+    _LINEAGE_COMMENTS_QUERY,
+    _INSERT_INFO_INTO_METADATA,
 )
 
-TAXONOMY_CREATE_CONSTRAINTS_AND_IDXS_QUERIES: tuple = (
-    CREATE_TAXONOMY_TAX_NAME_IDX_QUERY,
-    ADD_CONSTRAINTS_TAXONOMY_QUERY,
-    ADD_NOT_NULL_CONSTRAINT_TAXONOMY_QUERY,
-    CREATE_TRGM_IDX_ON_TAXONOMY,
+_TAXONOMY_CREATE_CONSTRAINTS_AND_IDXS_QUERIES: tuple = (
+    _CREATE_TAXONOMY_TAX_NAME_IDX_QUERY,
+    _ADD_CONSTRAINTS_TAXONOMY_QUERY,
+    _ADD_NOT_NULL_CONSTRAINT_TAXONOMY_QUERY,
+    _CREATE_TRGM_IDX_ON_TAXONOMY,
 )
 
-LINEAGE_CREATE_CONSTRAINTS_AND_IDXS_QUERIES: tuple = (
-    CREATE_LINEAGE_IDXS_QUERY,
-    ADD_CONSTRAINTS_LINEAGE_QUERY,
-    ADD_NOT_NULL_CONSTRAINTS_LINEAGE_QUERY,
+_LINEAGE_CREATE_CONSTRAINTS_AND_IDXS_QUERIES: tuple = (
+    _CREATE_LINEAGE_IDXS_QUERY,
+    _ADD_CONSTRAINTS_LINEAGE_QUERY,
+    _ADD_NOT_NULL_CONSTRAINTS_LINEAGE_QUERY,
 )
 
 CREATE_CONSTRAINTS_AND_IDXS_FOR_TAXONOMY_AND_LINEAGE_QUERIES: tuple = (
-    TAXONOMY_CREATE_CONSTRAINTS_AND_IDXS_QUERIES,
-    LINEAGE_CREATE_CONSTRAINTS_AND_IDXS_QUERIES,
+    _TAXONOMY_CREATE_CONSTRAINTS_AND_IDXS_QUERIES,
+    _LINEAGE_CREATE_CONSTRAINTS_AND_IDXS_QUERIES,
 )
 
 UNIPROT_KB_AND_TAXONOMY_VALIDATION_QUERIES: tuple = (
-    SUBSTITUTE_OUTDATED_NCBI_IDS_IN_UNIPROT_KB_QUERY,
-    CREATE_NCBI_ID_FKEY_UNIPROT_KB,
-    ADD_NOT_NULL_UNIPROT_KB_QUERY,
-    DROP_UNUSED_IDXS_QUERY,
-    DROP_MERGED_ID_QUERY,
-    ADD_PK_CONSTRAINT_UNIPROT_KB_QUERY,
-    CREATE_IDXS_UNIPROT_KB_QUERY,
+    _SUBSTITUTE_OUTDATED_NCBI_IDS_IN_UNIPROT_KB_QUERY,
+    _CREATE_NCBI_ID_FKEY_UNIPROT_KB,
+    _ADD_NOT_NULL_UNIPROT_KB_QUERY,
+    _DROP_UNUSED_IDXS_QUERY,
+    _DROP_MERGED_ID_QUERY,
+    _ADD_PK_CONSTRAINT_UNIPROT_KB_QUERY,
+    _CREATE_IDXS_UNIPROT_KB_QUERY,
 )
