ad4c0f42851db48d65406675d3b1980de8cdbed2
Denis Matsuga <HitokiriDeepWeb@yandex.ru>
2025-12-21 20:07:28 +0400

    Increase timeouts

--- src/domain/services/queue_manager/config.py
+++ src/domain/services/queue_manager/config.py
@@ -5,5 +5,5 @@ from dataclasses import dataclass
 class QueueConfig:
     queue_max_size: int
     queue_workers_number: int
-    task_timeout: float = 40.0
-    join_timeout: float = 50.0
+    task_timeout: float = 1800.0
+    join_timeout: float = 1900.0
